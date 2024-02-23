be328028913b5629deebdb65e0466915aa95e8d2
Deekshita S Doli <90824715+thekshita@users.noreply.github.com>
2024-02-23 12:05:07 -0800

    updates

--- agent.py
+++ agent.py
@@ -15,7 +15,7 @@ CHAT_MODEL = 'gpt-3.5-turbo'
 class Agent:
 
     def __init__(self, option):
-        self.llm = ChatOpenAI(temperature=0, model_name=CHAT_MODEL)#, api_key=os.getenv('OPENAI_API_KEY'))
+        self.llm = ChatOpenAI(temperature=0, model_name=CHAT_MODEL, api_key=st.secrets['OPENAI_API_KEY'])#, api_key=os.getenv('OPENAI_API_KEY'))
         self.agent_executor = self.create_agent_executor(option)
 
     def create_agent_executor(self, option):
